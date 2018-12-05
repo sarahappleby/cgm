8eeb2b6f417bf8ef90205631016e63f9d8e97838
Sarah Appleby <sapple@ursa.roe.ac.uk>
2018-12-05 13:41:55 +0000

    added yt units

--- pipeline.py
+++ pipeline.py
@@ -6,6 +6,7 @@ import sys
 from astropy.cosmology import FlatLambdaCDM
 import astropy.io.ascii as ascii
 from generate_spectra_pygad_trident import generate_trident_spectrum
+from yt.units.yt_array import YTArray
 
 model = sys.argv[1]
 snap = sys.argv[2]
@@ -15,24 +16,25 @@ infile = '/home/rad/data/'+model+'/'+wind+'/Groups/'+model+'_'+snap+'.hdf5'
 
 # load information about COS-Halos galaxies
 cos_data = ascii.read('/home/sapple/cgm/cos_data.dat')
-cos_b = cos_data['b(kpc)']
-cos_M = cos_data['log10(M_*)(Msun)']
+cos_b = YTArray(cos_data['b(kpc)'], 'kpc')
+cos_M = np.array(cos_data['log10(M_*)(Msun)'])
 
 sim = caesar.load(infile)
 ds = yt.load(snapfile)
 
 h = sim.simulation.hubble_constant
-hubble = h*1.e2
+co = yt.utilities.cosmology.Cosmology()
+hubble = co.hubble_constant.in_units('km/s/kpc')
 c = yt.units.c.in_units('km/s')
 
 line_tri = 'H I 1216'
 lambda_rest = 1215.6701
 
 gals = sim.central_galaxies
-stellar_masses = np.array([gals[i].masses['stellar'].in_units('Msun') for i in range(len(gals))])
+stellar_masses = YTArray([gals[i].masses['stellar'].in_units('Msun') for i in range(len(gals))], 'Msun')
 stellar_masses = np.log10(stellar_masses)
-positions = np.array([gals[i].pos.in_units('kpc/h') for i in range(len(gals))])
-vels = np.array([gals[i].vel for i in range(len(gals))])
+positions = YTArray([gals[i].pos.in_units('kpc/h') for i in range(len(gals))], 'kpc/h')
+vels = YTArray([gals[i].vel.in_units('km/s') for i in range(len(gals))], 'km/s')
 
 # example used in Ford et al 2016
 # COS-halos galaxy with M=10**10.2 Msun and b = 18.26 kpc = 26.85 kpc/h
@@ -54,10 +56,12 @@ for i in range(len(cos_M)):
 				
 		rolled = np.roll(range(3), -1)
 		for ax in range(3): 
-			los = np.array(pos_sample[j]); los[ax] = ds.domain_left_edge[ax]; los[rolled[ax]] += cos_b[i]
-			vgal = vels_sample[j] + 1.e-3*hubble*pos_sample[j][2]
-			generate_trident_spectrum(ds, line_tri, los, line_tri + 'cos_'+str(i)+'_gal_'+str(j)+'_x_plus', lambda_rest)
+			ray_start = pos_sample[j].copy(); ray_start[ax] = ds.domain_left_edge[ax]; ray_start[rolled[ax]] += cos_b[i]
+			ray_end = pos_sample[j].copy(); ray_end[ax] = ds.domain_right_edge[ax]; ray_end[rolled[ax]] += cos_b[i]
+			vgal = vels_sample[j] + hubble*pos_sample[j][2]
+			generate_trident_spectrum(ds, line_tri, ray_start, ray_end, line_tri + 'cos_'+str(i)+'_gal_'+str(j)+'_x_plus', lambda_rest)
 
-			los = np.array(pos_sample[j]); los[ax] = ds.domain_left_edge[ax]; los[rolled[ax]] -= cos_b[i]
-               	 	vgal = vels_sample[j] + 1.e-3*hubble*pos_sample[j][2]
-                	generate_trident_spectrum(ds, line_tri, los, line_tri + 'cos_'+str(i)+'_gal_'+str(j)+'_x_minus', lambda_rest)     
+			ray_start = pos_sample[j].copy(); ray_start[ax] = ds.domain_left_edge[ax]; ray_start[rolled[ax]] -= cos_b[i]
+			ray_end = pos_sample[j].copy(); ray_end[ax] = ds.domain_right_edge[ax]; ray_end[rolled[ax]] -= cos_b[i]
+               	 	vgal = vels_sample[j] + hubble*pos_sample[j][2]
+                	generate_trident_spectrum(ds, line_tri, ray_start, ray_end, line_tri + 'cos_'+str(i)+'_gal_'+str(j)+'_x_minus', lambda_rest)     
