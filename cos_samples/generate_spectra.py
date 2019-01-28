correct units for pygad line of sight: (1+z) factor

cos_samples/generate_spectra.py
@@ -1,9 +1,9 @@
-import matplotli
+import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
 import numpy as np 
-import pygad
+import pygad as pg
 import gc
 import os
 import h5py
@@ -71,7 +71,7 @@ def generate_trident_spectrum(ds, line_list, ray_start, ray_end, spec_name, lamb
     del ray; gc.collect()
     return
 
-def generate_pygad_spectrum(s, los, line, lambda_rest, vbox, periodic_vel, Nbins, vpos, c, spec_name, save_dir):
+def generate_pygad_spectrum(s, los, line, lambda_rest, vbox, periodic_vel, Nbins, snr, vpos, c, spec_name, save_dir):
 	if os.path.isfile(save_dir+'/spectra/{}.h5'.format(spec_name)):
       		check = h5py.File(save_dir+'/spectra/{}.h5'.format(spec_name), 'r')
 		if line + '_col_densities' in check.keys(): return
@@ -96,11 +96,11 @@ def generate_pygad_spectrum(s, los, line, lambda_rest, vbox, periodic_vel, Nbins
 			if velocities[i] > vbox: taus[i-npix_periodic] += taus[i]
 
 	plt.plot(velocities, fluxes)
-    plt.axvline(vpos, linewidth=1, c='k')
-    plt.xlabel('Velocity (km/s)')
-    plt.ylabel('Flux')
-    plt.savefig(save_dir+'/plots/'+spec_name+'_'+line+'.png')
-    plt.clf()
+        plt.axvline(vpos, linewidth=1, c='k')
+        plt.xlabel('Velocity (km/s)')
+        plt.ylabel('Flux')
+        plt.savefig(save_dir+'/plots/'+spec_name+'_'+line+'.png')
+        plt.clf()
 
 	with h5py.File(save_dir+'/spectra/{}.h5'.format(spec_name), 'a') as hf:
 	    hf.create_dataset(line+'_flux', data=np.array(fluxes))

cos_samples/pipeline.py
@@ -7,7 +7,9 @@ import yt
 import caesar
 from yt.units.yt_array import YTArray, YTQuantity
 import pygad as pg
+from pyigm.cgm import cos_halos as pch
 
+from generate_spectra import generate_pygad_spectrum
 
 model = sys.argv[1]
 snap = sys.argv[2]
@@ -18,6 +20,15 @@ lambda_rest = float(filter(str.isdigit, line))
 snapfile = '/home/rad/data/'+model+'/'+wind+'/snap_'+model+'_'+snap+'.hdf5'
 infile = '/home/rad/data/'+model+'/'+wind+'/Groups/'+model+'_'+snap+'.hdf5'
 save_dir = '/home/sapple/cgm/cos_samples/pygad/periodic/kpch/'
+
+save_dir = '/home/sapple/cgm/cos_samples/test/'
+
+cos_halos = pch.COSHalos()
+cos_rho = []
+for cos in cos_halos:
+        cos = cos.to_dict()
+        cos_rho.append(cos['rho'])
+cos_rho = pg.UnitArr(cos_rho, 'kpc')
 
 ds = yt.load(snapfile)
 s = pg.Snap(snapfile)
@@ -33,9 +44,12 @@ sigma_vel = 6.
 Nbins = int(np.rint(vbox / sigma_vel))
 
 
-cos_sample = h5py.File(sample_dir+'/samples/cos_galaxy_'+str(cos_id)+'_sample_data.h5', 'r')
+cos_sample = h5py.File(save_dir+'/samples/cos_galaxy_'+str(cos_id)+'_sample_data.h5', 'r')
 gal_ids = cos_sample['gal_ids'].value
-pos_sample = cos_sample['position'].value
+# we can't have the line of sight as a pygad UnitArr because it can't convert between kpc/h and ckpc/h_0
+# so instead we convert to default units of s['pos']
+# hence kpc/h and the factor of (1+z) is necessary
+pos_sample = cos_sample['position'].value*(1.+s.redshift)
 vgal_position_sample = cos_sample['vgal_position'].value
 cos_sample.close()	
 
@@ -49,27 +63,27 @@ for i in range(len(gal_ids)):
 	print 'Converting galaxy positions from kpc/h to ckpc/h_0 for pygad'
 
 	spec_name = gal_name + 'x_plus'
-	los = pos_sample[i][:2].copy(); los[0] += cos_rho[cos_id].value
+	los = pos_sample[i][:2].copy(); los[0] += cos_rho[cos_id]
 	print 'In kpc/h: ' + str(los)
 	#los /= ((1. + s.redshift)*co.hubble_parameter(s.redshift).in_units('km/s/kpc'))
 	#los *= co.hubble_parameter(0.0).in_units('km/s/kpc')	
 	#print 'In ckpc/h_0: ' + str(los)
-	generate_pygad_spectrum(s, los, line, lambda_rest, vbox, periodic_vel, Nbins, vgal_position_sample[i], c, spec_name, save_dir)
+	generate_pygad_spectrum(s, los, line, lambda_rest, vbox, periodic_vel, Nbins, snr, vgal_position_sample[i], c, spec_name, save_dir)
 	
 	spec_name = gal_name + 'x_minus'
-	los = pos_sample[i][:2].copy(); los[0] -= cos_rho[cos_id].value
+	los = pos_sample[i][:2].copy(); los[0] -= cos_rho[cos_id]
 	#los /= ((1. + s.redshift)*co.hubble_parameter(s.redshift).in_units('km/s/kpc'))
 	#los *= co.hubble_parameter(0.0).in_units('km/s/kpc')
-	generate_pygad_spectrum(s, los, line, lambda_rest, vbox, periodic_vel, Nbins, vgal_position_sample[i], c, spec_name, save_dir)
+	generate_pygad_spectrum(s, los, line, lambda_rest, vbox, periodic_vel, Nbins, snr, vgal_position_sample[i], c, spec_name, save_dir)
 
 	spec_name = gal_name + 'y_plus'
-	los = pos_sample[i][:2].copy(); los[1] += cos_rho[cos_id].value
+	los = pos_sample[i][:2].copy(); los[1] += cos_rho[cos_id]
 	#los /= ((1. + s.redshift)*co.hubble_parameter(s.redshift).in_units('km/s/kpc'))
 	#los *= co.hubble_parameter(0.0).in_units('km/s/kpc')
-	generate_pygad_spectrum(s, los, line, lambda_rest, vbox, periodic_vel, Nbins, vgal_position_sample[i], c, spec_name, save_dir)
+	generate_pygad_spectrum(s, los, line, lambda_rest, vbox, periodic_vel, Nbins, snr, vgal_position_sample[i], c, spec_name, save_dir)
 	
 	spec_name = gal_name + 'y_minus'	
-	los = pos_sample[i][:2].copy(); los[1] -= cos_rho[cos_id].value
+	los = pos_sample[i][:2].copy(); los[1] -= cos_rho[cos_id]
 	#los /= ((1. + s.redshift)*co.hubble_parameter(s.redshift).in_units('km/s/kpc'))
 	#los *= co.hubble_parameter(0.0).in_units('km/s/kpc')
-	generate_pygad_spectrum(s, los, line, lambda_rest, vbox, periodic_vel, Nbins, vgal_position_sample[i], c, spec_name, save_dir)
+	generate_pygad_spectrum(s, los, line, lambda_rest, vbox, periodic_vel, Nbins, snr, vgal_position_sample[i], c, spec_name, save_dir)