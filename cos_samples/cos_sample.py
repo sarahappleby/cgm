351c2d1209cc37c151867c911fd39a435e9f39ff
Sarah Appleby <sarahappleby20@gmail.com>
2019-08-25 16:35:37 +0000

    changing to python 3 printing

--- cos_samples/cos_sample.py
+++ cos_samples/cos_sample.py
@@ -4,14 +4,22 @@ import numpy as np
 import caesar
 from pyigm.cgm import cos_halos as pch
 import yt
+import h5py
+import matplotlib.pyplot as plt
+
+def touch(path):
+    with open(path, 'a'):
+        os.utime(path, None)
 
 model = sys.argv[1]
 snap = sys.argv[2]
 wind = sys.argv[3]
 
-sample_dir = '/home/sapple/cgm/cos_samples/pygad/periodic/kpch/samples/'
+sample_dir = sys.argv[4]
+if not os.path.exists(sample_dir):
+	os.makedirs(sample_dir)
 
-infile = '/home/rad/data/'+model+'/'+wind+'/Groups/'+model+'_'+snap+'.hdf5'
+touch(sample_dir+ '/cos_galaxy_samples.h5')
 
 mass_range = 0.125
 
@@ -24,70 +32,85 @@ for cos in cos_halos:
 	cos_M.append(cos['galaxy']['stellar_mass'])
 	cos_ssfr.append(cos['galaxy']['ssfr'])
 	cos_rho.append(cos['rho'])
-cos_rho = YTArray(cos_rho, 'kpc')
+cos_rho = yt.YTArray(cos_rho, 'kpc')
 
-print 'Loaded COS-Halos survey data'
+plt.scatter(cos_M, np.log10(cos_ssfr), c='k', marker='x', label='COS-Halos')
 
-sim = caesar.load(infile)
-gals = sim.central_galaxies
+print('Loaded COS-Halos survey data')
+
+infile = '/home/rad/data/'+model+'/'+wind+'/Groups/'+model+'_'+snap+'.hdf5'
+sim = caesar.load(infile, LoadHalo=False)
+gal_cent = np.array([i.central for i in sim.galaxies])
 
 co = yt.utilities.cosmology.Cosmology()
-hubble = co.hubble_parameter(ds.current_redshift).in_units('km/s/kpc')
+hubble = co.hubble_parameter(sim.simulation.redshift).in_units('km/s/kpc')
 
-stellar_masses = yt.YTArray([gals[i].masses['stellar'].in_units('Msun') for i in range(len(gals))], 'Msun')
-sfr = np.array([gals[i].sfr.in_units('Msun/yr') for i in range(len(gals))])
-ssfr = sfr / stellar_masses
-positions = yt.YTArray([gals[i].pos.in_units('kpc/h') for i in range(len(gals))], 'kpc/h')
-vels = yt.YTArray([gals[i].vel.in_units('km/s') for i in range(len(gals))], 'km/s')
-stellar_masses = np.log10(stellar_masses)
-recession = positions.in_units('kpc')*hubble
-vgal_position = vels + recession
+gal_sm = yt.YTArray([sim.galaxies[i].masses['stellar'].in_units('Msun') for i in range(len(sim.galaxies))], 'Msun')
+gal_sfr = yt.YTArray([sim.galaxies[i].sfr.in_units('Msun/yr') for i in range(len(sim.galaxies))], 'Msun/yr')
+gal_ssfr = gal_sfr / gal_sm
+gal_pos = yt.YTArray([sim.galaxies[i].pos.in_units('kpc/h') for i in range(len(sim.galaxies))], 'kpc/h')
+gal_vels = yt.YTArray([sim.galaxies[i].vel.in_units('km/s') for i in range(len(sim.galaxies))], 'km/s')
+gal_sm = np.log10(gal_sm)
+gal_recession = gal_pos.in_units('kpc')*hubble
+gal_vgal_pos = gal_vels + gal_recession
 
-print 'Loaded caesar galaxy data from model ' + model + ' snapshot ' + snap
+print('Loaded caesar galaxy data from model ' + model + ' snapshot ' + snap)
+
+choose_mask = np.ones(len(gal_sm))
+
+gal_ids = np.zeros(44*5)
+mass = np.zeros(44*5)
+ssfr = np.zeros(44*5)
+pos = np.zeros((44*5, 3))
+vgal_pos = np.zeros((44*5, 3))
 
 for cos_id in range(44):
-	if os.path.isfile(sample_dir+'cos_galaxy_'+str(cos_id)+'_sample_data.h5'):
-		continue
-	else:
+        ids = range(cos_id*5, (cos_id+1)*5)
+	print('\nFinding the caesar galaxies in the mass and ssfr range of COS Halos galaxy ' + str(cos_id))
+	
+	mass_mask = (gal_sm > (cos_M[cos_id] - mass_range)) & (gal_sm < (cos_M[cos_id] + mass_range))
+	stop = False
+	init = 0.1
+	while not stop:
+		ssfr_mask = (gal_ssfr > (1. - init)*cos_ssfr[cos_id]) & (gal_ssfr < (1. + init)*cos_ssfr[cos_id])
+		mask = mass_mask * ssfr_mask * gal_cent * choose_mask
+		indices = np.where(mask == True)[0]
+		if len(indices) < 5.: 
+			init += 0.1
+			continue
+		else:
+			stop = True
+			continue
 
-		print '\nFinding the caesar galaxies in the mass and ssfr range of COS Halos galaxy ' + str(cos_id)
-		
-		mass_mask = (stellar_masses > (cos_M[cos_id] - mass_range)) & (stellar_masses < (cos_M[cos_id] + mass_range))
-		stop = False
-		init = 0.1
-		while not stop:
-			ssfr_mask = (ssfr > (1. - init)*cos_ssfr[cos_id]) & (ssfr < (1. + init)*cos_ssfr[cos_id])
-			mask = mass_mask * ssfr_mask
-			indices = np.where(mask == True)[0]
-			if len(indices) < 5.: 
-				init += 0.1
-				continue
-			else:
-				stop = True
-				continue
+	# choose 5 of the galaxies that satisfy the COS-Halos galaxy's conditions
+	choose = np.sort(np.random.choice(range(len(indices)), 5, replace=False))
+	print('Chosen galaxies ' + str(indices[choose]))
+	gal_ids[ids] = indices[choose]
+        mass[ids] = gal_sm[indices[choose]]
+	ssfr[ids] = gal_ssfr[indices[choose]]
+	pos[ids] = gal_pos[indices[choose]]
+	vgal_pos[ids] = gal_vgal_pos[indices[choose]]
 
-		# choose 5 of the galaxies that satisfy the COS-Halos galaxy's conditions
-		choose = np.sort(np.random.choice(range(len(indices)), 5, replace=False))
-		print 'Chosen galaxies ' + str(indices[choose])
-		gal_ids = indices[choose]
+	# do not repeat galaxies
+        choose_mask[indices[choose]] = np.zeros(5)
 
-		mass_sample = stellar_masses[indices[choose]]
-		ssfr_sample = ssfr[indices[choose]]
-		pos_sample = positions[indices[choose]]
-		vels_sample = vels[indices[choose]]
-		vgal_position_sample = vgal_position[indices[choose]]
+with h5py.File(sample_dir+'/'+model+'_'+wind+'_cos_galaxy_sample.h5', 'a') as hf:
+        hf.create_dataset('gal_ids', data=np.array(gal_ids))
+        hf.create_dataset('mass', data=np.array(mass))
+        hf.create_dataset('ssfr', data=np.array(ssfr))
+        hf.create_dataset('position', data=np.array(pos))
+        hf.create_dataset('vgal_position', data=np.array(vgal_pos))
+        hf.attrs['pos_units'] = 'kpc/h'
+        hf.attrs['mass_units'] = 'Msun'
+        hf.attrs['ssfr_units'] = 'Msun/yr'
+        hf.attrs['vel_units'] = 'km/s'
 
-		del gals, stellar_masses, ssfr, mass_mask, ssfr_mask
+ssfr[np.where(ssfr == 0.)] = 1.e-14
+plt.scatter(mass, np.log10(ssfr), s=2., c='b', label='Simba')
+plt.xlabel('log M*')
+plt.ylabel('log sSFR')
+plt.ylim(-14.5, )
+plt.legend()
+plt.savefig(sample_dir + '/'+model+'_'+wind+'_sample_plot.png')
+plt.clf()
 
-		with h5py.File(sample_dir+'/samples/cos_galaxy_'+str(cos_id)+'_sample_data.h5', 'w') as hf:
-			hf.create_dataset('mask', data=np.array(mask))
-			hf.create_dataset('gal_ids', data=np.array(indices[choose]))
-			hf.create_dataset('mass', data=np.array(mass_sample))
-			hf.create_dataset('ssfr', data=np.array(ssfr_sample))
-			hf.create_dataset('position', data=np.array(pos_sample))
-			hf.create_dataset('vgal_position', data=np.array(vgal_position_sample))
-			hf.attrs['pos_units'] = 'kpc/h'
-			hf.attrs['mass_units'] = 'Msun'
-			hf.attrs['ssfr_units'] = 'Msun/yr'
-			hf.attrs['vel_units'] = 'km/s'
-		hf.close()
--- cos_samples/pipeline.py
+++ cos_samples/pipeline.py
@@ -17,11 +17,12 @@ cos_id = int(sys.argv[4])
 line = sys.argv[5]
 lambda_rest = float(filter(str.isdigit, line))
 
+ids = range(cos_id*5, (cos_id+1)*5)
+
 snapfile = '/home/rad/data/'+model+'/'+wind+'/snap_'+model+'_'+snap+'.hdf5'
 infile = '/home/rad/data/'+model+'/'+wind+'/Groups/'+model+'_'+snap+'.hdf5'
-save_dir = '/home/sapple/cgm/cos_samples/pygad/periodic/kpch/'
 
-save_dir = '/home/sapple/cgm/cos_samples/test/'
+save_dir = '/home/sapple/cgm/cos_samples/samples/'
 
 # need to get the impact parameters from the COS-Halos survey data:
 cos_halos = pch.COSHalos()
@@ -45,27 +46,27 @@ sigma_vel = 6. # km/s
 Nbins = int(np.rint(vbox / sigma_vel))
 
 # Load in data for the sample galaxies corresponding to this COS-Halos galaxy
-cos_sample = h5py.File(save_dir+'/samples/cos_galaxy_'+str(cos_id)+'_sample_data.h5', 'r')
-gal_ids = cos_sample['gal_ids'].value
+with h5py.File(save_dir+'/'+model+'_'+wind+'_cos_galaxy_sample.h5', 'r') as cos_sample:
 
-# we can't have the line of sight as a pygad UnitArr because it can't convert between kpc/h and ckpc/h_0
-# so instead we convert to default units of s['pos']
-# hence kpc/h and the factor of (1+z) is necessary
-pos_sample = cos_sample['position'].value*(1.+ds.current_redshift)
-vgal_position_sample = cos_sample['vgal_position'].value
-cos_sample.close()	
+	gal_ids = (cos_sample['gal_ids'].value)[ids]
+
+	# we can't have the line of sight as a pygad UnitArr because it can't convert between kpc/h and ckpc/h_0
+	# so instead we convert to default units of s['pos']
+	# hence kpc/h and the factor of (1+z) is necessary
+	pos_sample = (cos_sample['position'].value*(1.+ds.current_redshift))[ids]
+        vgal_position_sample = (cos_sample['vgal_position'].value)[ids][:, 2]
 
 # Load in snapshot for pygad spectra generation:
 s = pg.Snap(snapfile)
 
 # Generate spectra for each line of sight:
 for i in range(len(gal_ids)):
-	print 'Generating spectra for sample galaxy ' + str(gal_ids[i])
-	gal_name = 'cos_galaxy_'+str(cos_id)+'_sample_galaxy_' + str(gal_ids[i]) + '_'
+	print('Generating spectra for sample galaxy ' + str(gal_ids[i]))
+	gal_name = 'sample_galaxy_' + str(gal_ids[i]) + '_'
 
 	spec_name = gal_name + 'x_plus'
 	los = pos_sample[i][:2].copy(); los[0] += cos_rho[cos_id]
-	print 'In kpc/h: ' + str(los)
+	print('In kpc/h: ' + str(los))
 	generate_pygad_spectrum(s, los, line, lambda_rest, vbox, periodic_vel, Nbins, snr, vgal_position_sample[i], vel_range, spec_name, save_dir)
 	
 	spec_name = gal_name + 'x_minus'
