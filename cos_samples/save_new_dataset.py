a7532cb03fd49a4def348b885a910eddcf616184
Sarah Appleby <sarahappleby20@gmail.com>
2020-01-27 13:06:22 +0000

    new sub dataset methods, tests on m50n512

--- cos_samples/save_new_dataset.py
+++ cos_samples/save_new_dataset.py
@@ -2,80 +2,95 @@
 # Based on scripts by Sydney Lower and Chris Lovell
 # https://gist.github.com/christopherlovell/5a504c2c9d26efb6e073324d80c755a6 
 
+
+
 import h5py
 import caesar
 import numpy as np
 
-model = 'm100n1024'
-wind = 's50'
-survey = 'dwarfs'
-verbose = 2
+def make_new_dataset(snapfile, output_file, plist, verbose):
+    ignore_fields = []
+    with h5py.File(snapfile, 'r') as in_file:
 
-if survey == 'dwarfs':
-    snap = '151'
-elif survey == 'halos':
-    snap = '137'
+        for ptype in ['PartType0']:
 
-data_dir = '/home/rad/data/'+model+'/'+wind+'/'
-output_dir = '/home/sapple/cgm/cos_samples/cos_'+survey+'/samples/'
-output_file = output_dir + model+'_'+wind+'_'+snap+'.hdf5'
+            pidx = int(ptype[8:]) # get particle type index
 
+            for k in in_file[ptype]: # loop through fields
 
-with h5py.File(output_dir+'particles_needed.h5', 'r') as f:
-    plist = np.array(f['plist'][:], dtype='int')
+                if k in ignore_fields:
+                    if verbose > 1: print(k, ' skipped...')
+                    continue
 
-numpart = np.zeros(6, dtype='int')
-numpart[0] = len(plist)
+                if verbose > 1: print(ptype, k)
 
-snapfile = data_dir + 'snap_'+model+'_'+snap+'.hdf5'
-with h5py.File(snapfile, 'r') as in_file:
-    header = in_file['Header']
-
-    with h5py.File(output_file, 'a') as out_file:
-        if 'Header' not in out_file:
-            out_file.copy(header, 'Header')
-            out_file['Header'].attrs['NumPart_ThisFile'] = numpart
-            out_file['Header'].attrs['NumPart_Total'] = numpart
-        for group in ['PartType0']:
-            if group not in out_file:
-                out_file.create_group(group)
+                # load a given field (the bottleneck)
+                temp_dset = in_file[ptype][k][:]
+                if verbose > 1: print(temp_dset.shape)
 
 
-ignore_fields = []
+                with h5py.File(output_file, 'a') as out_file:
 
-with h5py.File(snapfile, 'r') as in_file:
+                    if '%s/%s'%(ptype,k) in output_file:
+                        if verbose > 1: print("dataset already exists. replacing...")
+                        del output_file[ptype][k]
 
-    for ptype in ['PartType0']:
-        
-        pidx = int(ptype[8:]) # get particle type index
+                    out_file[ptype][k] = temp_dset[plist]
 
-        for k in in_file[ptype]: # loop through fields
+                    temp = out_file['Header'].attrs['NumPart_ThisFile']
+                    temp[pidx] = len(plist)
+                    out_file['Header'].attrs['NumPart_ThisFile'] = temp
 
-            if k in ignore_fields:
-                if verbose > 1: print(k, ' skipped...')
-                continue
-
-            if verbose > 1: print(ptype, k)
-
-            # load a given field (the bottleneck)
-            temp_dset = in_file[ptype][k][:]
-            if verbose > 1: print(temp_dset.shape)
+                    temp = out_file['Header'].attrs['NumPart_Total']
+                    temp[pidx] = len(plist)
+                    out_file['Header'].attrs['NumPart_Total'] = temp
 
 
-            with h5py.File(output_file, 'a') as out_file:
-                
-                if '%s/%s'%(ptype,k) in output_file:
-                    if verbose > 1: print("dataset already exists. replacing...")
-                    del output_file[ptype][k]
+def prepare_out_file(snapfile, output_file, numpart):
+    with h5py.File(snapfile, 'r') as in_file:
+        header = in_file['Header']
 
-                out_file[ptype][k] = temp_dset[plist]
+        with h5py.File(output_file, 'a') as out_file:
+            if 'Header' not in out_file:
+                out_file.copy(header, 'Header')
+                out_file['Header'].attrs['NumPart_ThisFile'] = numpart
+                out_file['Header'].attrs['NumPart_Total'] = numpart
+            for group in ['PartType0']:
+                if group not in out_file:
+                    out_file.create_group(group)
 
-                temp = out_file['Header'].attrs['NumPart_ThisFile']
-                temp[pidx] = len(plist)
-                out_file['Header'].attrs['NumPart_ThisFile'] = temp
+if __name__ == '__main__':
 
-                temp = out_file['Header'].attrs['NumPart_Total']
-                temp[pidx] = len(plist)
-                out_file['Header'].attrs['NumPart_Total'] = temp
+    model = 'm100n1024'
+    wind = 's50'
+    survey = 'halos'
+    verbose = 2
 
+    if survey == 'dwarfs':
+        snap = '151'
+    elif survey == 'halos':
+        snap = '137'
 
+    data_dir = '/home/rad/data/'+model+'/'+wind+'/'
+    snapfile = data_dir+'snap_'+model+'_'+snap+'.hdf5'
+
+    output_dir = '/home/sapple/cgm/cos_samples/cos_'+survey+'/samples/'
+    output_file = output_dir + model+'_'+wind+'_'+snap+'.hdf5'
+
+    sample_file = 'cos_'+survey+'/samples/m100n1024_s50_cos_'+survey+'_sample.h5'
+    with h5py.File(sample_file, 'r') as f:
+        gal_ids = np.array(f['gal_ids'][:], dtype='int')
+
+    plist = np.array([])
+    with h5py.File(output_dir+'particle_selection.h5', 'r') as f:
+        for i, gal in enumerate(gal_ids):
+            plist = np.append(plist, np.array(f['plist_'+str(i)+'_'+str(gal)][:], dtype='int'))
+
+    plist = np.unique(np.sort(plist)).astype('int')
+
+    numpart = np.zeros(6, dtype='int')
+    numpart[0] = len(plist)
+
+    prepare_out_file(data_dir+'', output_file, numpart)
+
+    make_new_dataset(snapfile, output_file, plist, verbose)
--- cos_samples/spectrum_los_particles.py
+++ cos_samples/spectrum_los_particles.py
@@ -5,55 +5,73 @@ import yt
 from yt.units.yt_array import YTArray, YTQuantity
 from generate_spectra import generate_pygad_spectrum
 import matplotlib.pyplot as plt
+from pygadgetreader import readsnap
+from save_new_dataset import *
+import os
 
 def vel_to_wave(vel, lambda_rest, c, z):
         return lambda_rest * (1.0 + z) * (vel / c + 1.)
 
 line = 'H1215'
 lambda_rest = 1215.
+los = np.array([25000, 25000]) # los in kpc/h
+model = 'm50n512'
+wind = 's50'
+snap = '151'
+sample_dir = './los_particles_test/'
+output_snap = sample_dir+'m50n512_s50_151.hdf5'
+
+data_dir = '/home/rad/data/'+model+'/'+wind+'/'
+snapfile = data_dir + 'snap_'+model+'_'+snap +'.hdf5'
+
+if not os.path.isfile(output_snap):
+
+    hsml = readsnap(snapfile, 'SmoothingLength', 'gas', suppress=1, units=1)  # in kpc/h
+    gas_pos = readsnap(snapfile, 'pos', 'gas', suppress=1, units=1) # in kpc/h
+
+    x_dist = np.abs(los[0] - gas_pos[:, 0])
+    y_dist = np.abs(los[1] - gas_pos[:, 1])
+    dist_mask = (x_dist < hsml) & (y_dist < hsml)
+    plist = np.arange(len(hsml))[dist_mask]
+
+    plist = np.unique(np.sort(plist))
+    numpart = np.zeros(6, dtype='int')
+    numpart[0] = len(plist)
+
+    prepare_out_file(snapfile, output_snap, numpart)
+    make_new_dataset(snapfile, output_snap, plist, 2)
+
+snapshots = [snapfile, output_snap]
+names = ['old', 'new']
+
+for i, snapshot in enumerate(snapshots):
+    # Load in snapshot for pygad spectra generation:
+    s = pg.Snapshot(snapshot)
 
 
-los = np.array([15092.04743875, 82958.41646495]) # los in kpc/h
-gal = 11486
+    # Get some info from yt:
+    ds = yt.load(snapshot)
+    co = yt.utilities.cosmology.Cosmology()
+    hubble = co.hubble_parameter(ds.current_redshift).in_units('km/s/kpc')
+    vbox = ds.domain_right_edge[2].in_units('kpc') * hubble / ds.hubble_constant / (1.+ds.current_redshift)
 
-sample_file = 'cos_dwarfs/samples/m100n1024_s50_cos_dwarfs_sample.h5'
-with h5py.File(sample_file, 'r') as f:
-    vpos = f['vgal_position'][0][2]
+    # Set some spectrum parameters:
+    periodic_vel = True
+    vel_range = 600. # km/s
+    sigma_vel = 6. # km/s
+    Nbins = int(np.rint(vbox / sigma_vel))
+    v_limits = [-600., vbox.value+600.]
 
-snapfile = '/home/sapple/cgm/cos_samples/cos_dwarfs/samples/m100n1024_s50_151.hdf5'
-#snapfile = '/home/rad/data/m100n1024/s50j7k/snap_m100n1024_151.hdf5'
+    taus, col_densities, dens, temps, metal_frac, vel, v_edges, restr_column = \
+            pg.analysis.absorption_spectra.mock_absorption_spectrum_of(s, los, line, v_limits, Nbins=Nbins, return_los_phys=True)
 
-# Load in snapshot for pygad spectra generation:
-s = pg.Snapshot(snapfile)
+    fluxes = np.exp(-1.*taus)
+    velocities = 0.5 * (v_edges[1:] + v_edges[:-1])
+    wavelengths = vel_to_wave(velocities, lambda_rest, np.array(pg.physics.cosmology.c.in_units_of('km/s')), s.redshift)
 
-
-# Get some info from yt:
-ds = yt.load(snapfile)
-co = yt.utilities.cosmology.Cosmology()
-hubble = co.hubble_parameter(ds.current_redshift).in_units('km/s/kpc')
-vbox = ds.domain_right_edge[2].in_units('kpc') * hubble / ds.hubble_constant / (1.+ds.current_redshift)
-
-# Set some spectrum parameters:
-snr = 12.
-periodic_vel = True
-vel_range = 600. # km/s
-sigma_vel = 6. # km/s
-Nbins = int(np.rint(vbox / sigma_vel))
-v_limits = [-600., vbox.value+600.]
-
-taus, col_densities, dens, temps, metal_frac, vel, v_edges, restr_column = \
-        pg.analysis.absorption_spectra.mock_absorption_spectrum_of(s, los, line, v_limits, Nbins=Nbins, return_los_phys=True)
-
-fluxes = np.exp(-1.*taus)
-velocities = 0.5 * (v_edges[1:] + v_edges[:-1])
-wavelengths = vel_to_wave(velocities, lambda_rest, np.array(pg.physics.cosmology.c.in_units_of('km/s')), s.redshift)
-
-plt.plot(velocities, fluxes)
-plt.axvline(vpos, lw=1, c='k')
-plt.axvline(vpos + vel_range , lw=1, c='k', ls='--')
-plt.axvline(vpos - vel_range , lw=1, c='k', ls='--')
-plt.xlabel('Velocity (km/s)')
-plt.ylabel('Flux')
-plt.savefig('test_snapshot.png')
-plt.clf()
+    plt.plot(velocities, fluxes)
+    plt.xlabel('Velocity (km/s)')
+    plt.ylabel('Flux')
+    plt.savefig('test_snapshot_'+names[i]+'.png')
+    plt.clf()
 
