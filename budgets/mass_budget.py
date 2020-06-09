34eb728ca9175f25cb23ee8c6eb27b78ac6d51a1
Sarah Appleby <sarahappleby20@gmail.com>
2020-06-09 14:02:48 +0100

    scaling to omega baryon

--- budgets/mass_budget.py
+++ budgets/mass_budget.py
@@ -8,6 +8,7 @@ photo_temp = 10.**4.5 # in K
 cold_temp = 1.e5
 hot_temp = 1.e6
 ism_density = 0.13 # hydrogen number density, cm**-3
+omega_b = 0.048
 
 data_dir = '/home/sarah/data/'
 snapfile = data_dir+'snap_m12.5n128_135.hdf5'
@@ -24,6 +25,7 @@ gal_ssfr = gal_sfr / gal_sm
 gal_sm = np.log10(gal_sm)
 gal_ssfr = np.log10(gal_ssfr)
 
+dm_mass = readsnap(snapfile, 'mass', 'dm', suppress=1, units=1) / h # in Mo)
 gas_mass = readsnap(snapfile, 'mass', 'gas', suppress=1, units=1) / h # in Mo
 gas_z = readsnap(snapfile, 'z', 'gas', suppress=1, units=1)
 gas_nh = readsnap(snapfile, 'nh', 'gas', suppress=1, units=1) # in g/cm^3
@@ -38,13 +40,15 @@ star_z = readsnap(snapfile, 'z', 'star', suppress=1, units=1)
 
 phases = ['Cool CGM (T < 10^5)', 'Warm CGM (10^5 < T < 10^6)', 'Hot CGM (T > 10^6)',
 		  'Cool CGM (T < Tphoto)', 'Warm CGM (Tphoto < T < Tvir)', 'Hot CGM (T > Tvir)', 
-		  'ISM', 'Wind', 'Dust', 'Stars', 'Total']
+		  'ISM', 'Wind', 'Dust', 'Stars', 'Dark matter', 'Total baryons']
 mass_budget = {phase: np.zeros(len(sim.central_galaxies)) for phase in phases}
 metal_budget = {phase: np.zeros(len(sim.central_galaxies)) for phase in phases}
+del metal_budget['Dark matter']
 
 for i in range(len(sim.central_galaxies)):
 	glist = sim.galaxies[i].halo.glist
 	slist = sim.galaxies[i].halo.slist
+	dmlist = sim.galaxies[i].halo.dmlist
 
 	cgm_gas_mask = gas_nh[glist] < ism_density
 	wind_mask = gas_delaytime[glist] > 0.
@@ -73,23 +77,34 @@ for i in range(len(sim.central_galaxies)):
 	mass_budget['Wind'][i] = np.sum(gas_mass[glist][wind_mask])
 	mass_budget['Dust'][i] = np.sum(dust_mass[glist][np.invert(wind_mask)])
 	mass_budget['Stars'][i] = np.sum(star_mass[slist])
+	mass_budget['Dark matter'][i] = np.sum(dm_mass[dmlist])
 	metal_budget['ISM'][i] = np.sum(gas_mass[glist][np.invert(cgm_gas_mask) & np.invert(wind_mask)] * gas_z[glist][np.invert(cgm_gas_mask) & np.invert(wind_mask)])
 	metal_budget['Wind'][i] = np.sum(gas_mass[glist][wind_mask] * gas_z[glist][wind_mask])
 	metal_budget['Dust'][i] = np.sum(dust_mass[glist][np.invert(wind_mask)] * dust_z[glist][np.invert(wind_mask)])
 	metal_budget['Stars'][i] = np.sum(star_mass[slist] * star_z[slist])
 
-	mass_budget['Total'][i] = np.sum(gas_mass[glist]) + np.sum(dust_mass[glist]) + np.sum(star_mass[slist])
-	metal_budget['Total'][i] = np.sum(gas_mass[glist] * gas_z[glist]) + np.sum(dust_mass[glist] * dust_z[glist]) + np.sum(star_mass[slist] * star_z[slist])
+	mass_budget['Total baryons'][i] = np.sum(gas_mass[glist]) + np.sum(dust_mass[glist]) + np.sum(star_mass[slist])
+	metal_budget['Total baryons'][i] = np.sum(gas_mass[glist] * gas_z[glist]) + np.sum(dust_mass[glist] * dust_z[glist]) + np.sum(star_mass[slist] * star_z[slist])
 
-mass_fractions = {k: p/mass_budget['Total'] for k, p in mass_budget.items()} 
-mass_fractions['Total'] = mass_budget['Total'].copy()
+available_mass_fractions = {k: p/mass_budget['Total baryons'] for k, p in mass_budget.items()} 
+available_mass_fractions['Total baryons'] = mass_budget['Total baryons'].copy()
+del available_mass_fractions['Dark matter']
+
+cosmic_baryons = (mass_budget['Total baryons'] + mass_budget['Dark matter']) * omega_b
+omega_mass_fractions = {k: p/cosmic_baryons for k, p in mass_budget.items()}
+omega_mass_fractions['Cosmic baryons'] = cosmic_baryons.copy()
+del omega_mass_fractions['Dark matter']
 
 with h5py.File(savedir+'mass_budget.h5', 'a') as hf: 
 	for k, p in mass_budget.items(): 
 		hf.create_dataset(k, data=np.array(p)) 
 
-with h5py.File(savedir+'mass_fractions.h5', 'a') as hf: 
-	for k, p in mass_fractions.items(): 
+with h5py.File(savedir+'available_mass_fraction.h5', 'a') as hf: 
+	for k, p in available_mass_fractions.items(): 
+		hf.create_dataset(k, data=np.array(p)) 
+
+with h5py.File(savedir+'omega_mass_fraction.h5', 'a') as hf: 
+	for k, p in omega_mass_fractions.items(): 
 		hf.create_dataset(k, data=np.array(p)) 
 
 with h5py.File(savedir+'metal_budget.h5', 'a') as hf: 
