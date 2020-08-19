e307d8ce151024ddd65afa7adffcb5ebacbd238d
Sarah Appleby <sarahappleby20@gmail.com>
2020-08-19 17:47:19 +0100

    tweaking wind plots as per romeel+daniele meeting

--- absorption_analysis/plot_path_absorption_winds.py
+++ absorption_analysis/plot_path_absorption_winds.py
@@ -20,11 +20,12 @@ if __name__ == '__main__':
     det_thresh = [0.2, 0.2, 0.1, 0.1, 0.1, 0.1] # check CIV with Rongmon, check NeVIII with Jessica?
 
     model = 'm50n512'
-    winds = ['s50j7k', 's50nox', 's50nojet', 's50noagn']
-    wind_labels = [r'$\textrm{Simba}$', r'$\textrm{No-Xray}$', r'$\textrm{No-jet}$', r'$\textrm{No-AGN}$']
-    ls = ['-', '--', (0, (3, 5, 1, 5, 1, 5)), ':']
-    markers = ['o', 'D', 's', 'v']
+    winds = ['s50j7k', 's50nox', 's50nojet']
+    wind_labels = [r'$\textrm{Simba}$', r'$\textrm{No-Xray}$', r'$\textrm{No-jet}$']
+    ls = ['-', '--', ':']
+    markers = ['o', 'D', 's']
     ylim = 0.5
+    xoffset = 0.035
     r200_scaled = True
 
     sim_colors, cos_colors = get_tol_colors()
@@ -44,10 +45,15 @@ if __name__ == '__main__':
     line_sim = Line2D([0,1],[0,1],ls=ls[0], marker=markers[0], color='grey')
     line_jet = Line2D([0,1],[0,1],ls=ls[1], marker=markers[1], color='grey')
     line_x = Line2D([0,1],[0,1],ls=ls[2], marker=markers[2], color='grey')
-    line_agn = Line2D([0,1],[0,1],ls=ls[3], marker=markers[3], color='grey')
 
-    leg = ax[0].legend([line_sim, line_jet, line_x, line_agn],wind_labels, loc=4, fontsize=12)
-    ax[0].add_artist(leg)
+    leg_winds = ax[0].legend([line_sim, line_jet, line_x],wind_labels, loc=4, fontsize=12)
+    ax[0].add_artist(leg_winds)
+
+    line_sf = Line2D([0,1],[0,1],ls='-', marker=None, color=sim_colors[0])
+    line_q = Line2D([0,1],[0,1],ls='-', marker=None, color=sim_colors[1])
+
+    leg_color = ax[0].legend([line_sf, line_q],['Simba SF', 'Simba Q'], loc=3, fontsize=12)
+    ax[0].add_artist(leg_color)
 
     for j, wind in enumerate(winds):
 
@@ -55,6 +61,17 @@ if __name__ == '__main__':
         sim_halos_plot_dict = read_dict_from_h5(sim_halos_file)
         sim_dwarfs_file = '/home/sapple/cgm/absorption_analysis/data/cos_dwarfs_'+model+'_'+wind+'_151_sim_path_abs_data.h5'
         sim_dwarfs_plot_dict = read_dict_from_h5(sim_dwarfs_file)
+
+        if j == 0:
+            sim_halos_plot_dict['plot_bins_sf'] -= xoffset
+            sim_halos_plot_dict['plot_bins_q'] -= xoffset
+            sim_dwarfs_plot_dict['plot_bins_sf'] -= xoffset
+            sim_dwarfs_plot_dict['plot_bins_q'] -= xoffset
+        elif j == 2:
+            sim_halos_plot_dict['plot_bins_sf'] += xoffset
+            sim_halos_plot_dict['plot_bins_q'] += xoffset
+            sim_dwarfs_plot_dict['plot_bins_sf'] += xoffset
+            sim_dwarfs_plot_dict['plot_bins_q'] += xoffset
 
         for i, survey in enumerate(cos_survey):
 
@@ -71,11 +88,12 @@ if __name__ == '__main__':
             l1 = ax[i].errorbar(sim_plot_dict['plot_bins_sf'], sim_plot_dict['path_abs_'+lines[i]+'_sf'],
                             yerr=sim_plot_dict['path_abs_'+lines[i]+'_cv_std_sf'],
                             c=sim_colors[0], markersize=6, marker=markers[j], ls=ls[j])
-            l2 = ax[i].errorbar(sim_plot_dict['plot_bins_q'], sim_plot_dict['path_abs_'+lines[i]+'_q'],
-                            yerr=sim_plot_dict['path_abs_'+lines[i]+'_cv_std_q'],
+            l1[-1][0].set_linestyle(ls[j])
+            empty_mask = ~np.isnan(sim_plot_dict['path_abs_'+lines[i]+'_q'])
+            l2 = ax[i].errorbar(sim_plot_dict['plot_bins_q'][empty_mask], sim_plot_dict['path_abs_'+lines[i]+'_q'][empty_mask],
+                            yerr=sim_plot_dict['path_abs_'+lines[i]+'_cv_std_q'][empty_mask],
                             c=sim_colors[1], markersize=6, marker=markers[j], ls=ls[j])
-            if i == 0:
-                leg2 = ax[i].legend([l1, l2], ['Simba SF', 'Simba Q'], loc='lower left', fontsize=10.5)
+            l2[-1][0].set_linestyle(ls[j])
 
             ax[i].annotate(label, xy=(x, 0.91), xycoords='axes fraction',size=12, 
                             bbox=dict(boxstyle='round', fc='white', edgecolor='lightgrey')) 
--- absorption_analysis/plot_rho_ew_winds.py
+++ absorption_analysis/plot_rho_ew_winds.py
@@ -17,11 +17,12 @@ if __name__ == '__main__':
     det_thresh = np.log10([0.2, 0.2, 0.1, 0.1, 0.1, 0.1]) # check CIV with Rongmon, check NeVIII with Jessica?
 
     model = 'm50n512'
-    winds = ['s50j7k', 's50nox', 's50nojet', 's50noagn']
-    wind_labels = [r'$\textrm{Simba}$', r'$\textrm{No-Xray}$', r'$\textrm{No-jet}$', r'$\textrm{No-AGN}$']
-    ls = ['-', '--', (0, (3, 5, 1, 5, 1, 5)), ':']
-    markers = ['o', 'D', 's', 'v']
+    winds = ['s50j7k', 's50nox', 's50nojet']
+    wind_labels = [r'$\textrm{Simba}$', r'$\textrm{No-Xray}$', r'$\textrm{No-jet}$']
+    ls = ['-', '--', ':']
+    markers = ['o', 'D', 's']
     ylim = 0.5
+    xoffset = 0.035
     r200_scaled = True
 
     sim_colors, cos_colors = get_tol_colors()
@@ -41,10 +42,15 @@ if __name__ == '__main__':
     line_sim = Line2D([0,1],[0,1],ls=ls[0], marker=markers[0], color='grey')
     line_jet = Line2D([0,1],[0,1],ls=ls[1], marker=markers[1], color='grey')
     line_x = Line2D([0,1],[0,1],ls=ls[2], marker=markers[2], color='grey')
-    line_agn = Line2D([0,1],[0,1],ls=ls[3], marker=markers[3], color='grey')
 
-    leg = ax[0].legend([line_sim, line_jet, line_x, line_agn],wind_labels, loc=4, fontsize=12)
-    ax[0].add_artist(leg)
+    leg_winds = ax[0].legend([line_sim, line_jet, line_x],wind_labels, loc=4, fontsize=12)
+    ax[0].add_artist(leg_winds)
+
+    line_sf = Line2D([0,1],[0,1],ls='-', marker=None, color=sim_colors[0])
+    line_q = Line2D([0,1],[0,1],ls='-', marker=None, color=sim_colors[1])
+
+    leg_color = ax[0].legend([line_sf, line_q],['Simba SF', 'Simba Q'], loc=3, fontsize=12)
+    ax[0].add_artist(leg_color)
 
     for j, wind in enumerate(winds):
 
@@ -53,6 +59,16 @@ if __name__ == '__main__':
         sim_dwarfs_file = '/home/sapple/cgm/absorption_analysis/data/cos_dwarfs_'+model+'_'+wind+'_151_sim_ew_med_data.h5'
         sim_dwarfs_plot_dict = read_dict_from_h5(sim_dwarfs_file)
 
+        if j == 0:
+            sim_halos_plot_dict['plot_bins_sf'] -= xoffset
+            sim_halos_plot_dict['plot_bins_q'] -= xoffset
+            sim_dwarfs_plot_dict['plot_bins_sf'] -= xoffset
+            sim_dwarfs_plot_dict['plot_bins_q'] -= xoffset
+        elif j == 2:
+            sim_halos_plot_dict['plot_bins_sf'] += xoffset
+            sim_halos_plot_dict['plot_bins_q'] += xoffset
+            sim_dwarfs_plot_dict['plot_bins_sf'] += xoffset
+            sim_dwarfs_plot_dict['plot_bins_q'] += xoffset
 
         for i, survey in enumerate(cos_survey):
         
@@ -69,12 +85,12 @@ if __name__ == '__main__':
             l1 = ax[i].errorbar(sim_plot_dict['plot_bins_sf'], sim_plot_dict['EW_'+lines[i]+'_med_sf'], 
                                 yerr=sim_plot_dict['EW_'+lines[i]+'_cosmic_std_sf'], 
                                 capsize=4, c=sim_colors[0], markersize=6, marker=markers[j], linestyle=ls[j], label='Simba SF')
-            l2 = ax[i].errorbar(sim_plot_dict['plot_bins_q'], sim_plot_dict['EW_'+lines[i]+'_med_q'], 
-                                yerr=sim_plot_dict['EW_'+lines[i]+'_cosmic_std_q'], 
+            l1[-1][0].set_linestyle(ls[j])
+            empty_mask = ~np.isnan(sim_plot_dict['EW_'+lines[i]+'_med_q'])
+            l2 = ax[i].errorbar(sim_plot_dict['plot_bins_q'][empty_mask], sim_plot_dict['EW_'+lines[i]+'_med_q'][empty_mask], 
+                                yerr=sim_plot_dict['EW_'+lines[i]+'_cosmic_std_q'][empty_mask], 
                                 capsize=4, c=sim_colors[1], markersize=6, marker=markers[j], linestyle=ls[j], label='Simba Q')
-            if j == 0:
-                if i == 0:
-                    leg3 = ax[i].legend([l1, l2], ['Simba SF', 'Simba Q'], fontsize=10.5, loc=3)
+            l2[-1][0].set_linestyle(ls[j])
 
             if j == 0:
                 ax[i].annotate(label, xy=(x, 0.91), xycoords='axes fraction',size=12,
