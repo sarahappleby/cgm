ac928a2a11ec9841b7d313f5b431b551b7131851
Sarah Appleby <sarahappleby20@gmail.com>
2021-10-13 15:44:10 +0100

    sample inclination check

--- absorption/ml_project/get_galaxy_sample.py
+++ absorption/ml_project/get_galaxy_sample.py
@@ -51,7 +51,9 @@ def ssfr_type_check(mstar, sfr):
     return sf_mask, gv_mask, q_mask
 
 # Randomly select ngals_each galaxies in each region
-def choose_gals(gal_ids, ngals_each):
+def choose_gals(gal_ids, ngals_each, seed=1):
+    if seed is not None:
+        np.random.seed(seed)
     ngals = len(gal_ids)
     chosen = np.random.choice(ngals, ngals_each)
     return gal_ids[chosen]
@@ -67,7 +69,7 @@ mass_bins = np.arange(min_m, min_m+nbins_m*delta_m, delta_m)
 ngals_each = 12
 nbins_ssfr = 3
 
-sample_dir = f'/disk01/sapple/cgm/absorption/ml_project/data/samples/'
+sample_dir = f'/disk04/sapple/cgm/absorption/ml_project/data/samples/'
 data_dir = f'/home/rad/data/{model}/{wind}/'
 sim =  caesar.load(f'{data_dir}Groups/{model}_{snap}.hdf5')
 co = yt.utilities.cosmology.Cosmology()
@@ -81,6 +83,8 @@ gal_ssfr = gal_sfr / gal_sm
 gal_ssfr = np.log10(gal_ssfr.value + 1e-14)
 gal_pos = yt.YTArray([sim.galaxies[i].pos.in_units('kpc/h') for i in range(len(sim.galaxies))], 'kpc/h')
 gal_vels = yt.YTArray([sim.galaxies[i].vel.in_units('km/s') for i in range(len(sim.galaxies))], 'km/s')
+gal_lgas = yt.YTArray([sim.galaxies[i].rotation['gas_L'].in_units('Msun*km*kpc/(h*s)') for i in range(len(sim.galaxies))], 'Msun*km*kpc/(h*s)')
+gal_lbaryon = yt.YTArray([sim.galaxies[i].rotation['baryon_L'].in_units('Msun*km*kpc/(h*s)') for i in range(len(sim.galaxies))], 'Msun*km*kpc/(h*s)')
 gal_sm = np.log10(gal_sm)
 gal_recession = gal_pos.in_units('kpc')*hubble
 gal_vgal_pos = gal_vels + gal_recession
@@ -119,9 +123,11 @@ with h5py.File(f'{sample_dir}{model}_{wind}_{snap}_galaxy_sample.h5', 'a') as hf
     hf.create_dataset('mass', data=np.array(gal_sm[gal_ids]))
     hf.create_dataset('ssfr', data=np.array(gal_ssfr[gal_ids]))
     hf.create_dataset('sfr', data=np.array(gal_sfr[gal_ids]))
-    hf.create_dataset('gas_frac', data=np.array(gal_gas_frac[gal_ids]))
+    hf.create_dataset('fgas', data=np.array(gal_gas_frac[gal_ids]))
     hf.create_dataset('position', data=np.array(gal_pos[gal_ids]))
     hf.create_dataset('vgal_position', data=np.array(gal_vgal_pos[gal_ids]))
+    hf.create_dataset('L_gas', data=np.array(gal_lgas[gal_ids]))
+    hf.create_dataset('L_baryon', data=np.array(gal_lbaryon[gal_ids]))
     hf.create_dataset('halo_r200', data=np.array(halo_r200))
     hf.create_dataset('halo_pos', data=np.array(halo_pos))
     hf.attrs['pos_units'] = 'kpc/h'
--- absorption/ml_project/plot_galaxy_sample.py
+++ absorption/ml_project/plot_galaxy_sample.py
@@ -18,7 +18,10 @@ def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
     return new_cmap
 
 cmap = plt.get_cmap('plasma')
-cmap = truncate_colormap(cmap, 0., 0.95)
+cmap = truncate_colormap(cmap, 0.1, 0.9)
+
+sf_cmap = plt.get_cmap('jet_r')
+sf_cmap = truncate_colormap(sf_cmap, 0.1, 0.9)
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif', size=15)
@@ -29,29 +32,106 @@ if __name__ == '__main__':
     wind = sys.argv[2]
     snap = sys.argv[3]
 
-    sample_dir = f'/disk01/sapple/cgm/absorption/ml_project/data/samples/'
+    sample_dir = f'/disk04/sapple/cgm/absorption/ml_project/data/samples/'
     with h5py.File(f'{sample_dir}{model}_{wind}_{snap}_galaxy_sample.h5', 'r') as sf:
         gal_sm = sf['mass'][:]
         gal_sfr = sf['sfr'][:]
+        gal_fgas = np.log10(sf['fgas'][:] + 1e-3)
+        gal_Lbaryon = sf['L_baryon'][:]
+
+    z = np.array([0, 0, 1])
+    Lbaryon_norm = gal_Lbaryon / np.linalg.norm(gal_Lbaryon, axis=1)[:, None]
+    alpha = np.degrees(np.arccos([np.dot(z, i) for i in Lbaryon_norm]))
+
+    # for face-on galaxies at 90 degrees and edge on galaxies at 0 degrees:
+    #inclination = np.abs(alpha - 90)
+    #for i in range(len(inclination)):
+    #        if (inclination[i] > 90) & (inclination[i] < 180):
+    #        inclination[i] = np.abs(inclination[i] - 180)
+
+    # for face-on galaxies at 0 degrees and edge on galaxies at 90 degrees:
+    inclination = alpha.copy()
+    for i in range(len(inclination)):
+        if (alpha[i] > 90) & (alpha[i] < 180):
+            inclination[i] = 180 - alpha[i]
 
     delta_m = 0.25
     min_m = 10.
     nbins_m = 6
     mass_bins = np.arange(min_m, min_m+nbins_m*delta_m, delta_m)
 
-    sm_line = np.arange(9.5, 12., 0.5)
+    sm_line = np.arange(9.5, 12.5, 0.5)
     sf_line = sfms_line(sm_line)
     q_line = sfms_line(sm_line, b=-8.7)
 
-    plt.plot(sm_line, sf_line, ls='--', lw=1.8, c='k')
-    plt.plot(sm_line, q_line, ls='--', lw=1.8, c='k')
+    plt.plot(sm_line, sf_line, ls='--', lw=1.3, c='dimgray')
+    plt.plot(sm_line, q_line, ls='--', lw=1.3, c='dimgray')
+    plt.text(11.55, 1.1, 'SF')
+    plt.text(11.55, 0.3, 'GV')
+    plt.text(11.55, -0.7, 'Q')
     for i in range(nbins_m + 1):
-        plt.axvline(min_m+i*delta_m, ls=':', lw=1.8, c='k')
-    plt.scatter(gal_sm, np.log10(gal_sfr + 1e-3))
+        plt.axvline(min_m+i*delta_m, ls=':', lw=1.5, c='darkgray')
+    im = plt.scatter(gal_sm, np.log10(gal_sfr + 1e-3), c=inclination, cmap=cmap, s=5, marker='o')
+    plt.colorbar(im, label=r'$i\ (^\circ)$')
+    plt.clim(90, 0)
     plt.xlim(9.75,11.75)
-    plt.ylim(-3.5, 2.)
+    plt.ylim(-3.5, 1.5)
+    plt.xlabel(r'$\log\ (M_{\star} / M_{\odot})$')
+    plt.ylabel(r'$\textrm{log} ({\rm SFR} / M_{\odot}{\rm yr}^{-1})$')
+    plt.savefig(f'{sample_dir}{model}_{wind}_{snap}_inclination.png')
+    plt.clf()
+
+    plt.plot(sm_line, sf_line, ls='--', lw=1.3, c='dimgray')
+    plt.plot(sm_line, q_line, ls='--', lw=1.3, c='dimgray')
+    plt.text(11.55, 1.1, 'SF')
+    plt.text(11.55, 0.3, 'GV')
+    plt.text(11.55, -0.7, 'Q')
+    for i in range(nbins_m + 1):
+        plt.axvline(min_m+i*delta_m, ls=':', lw=1.5, c='darkgray')
+    im = plt.scatter(gal_sm, np.log10(gal_sfr + 1e-3), c=np.cos(alpha), cmap=cmap, s=5, marker='o')
+    plt.colorbar(im, label=r'${\rm cos}\ i$')
+    plt.clim(1, -1)
+    plt.xlim(9.75,11.75)
+    plt.ylim(-3.5, 1.5)
+    plt.xlabel(r'$\log\ (M_{\star} / M_{\odot})$')
+    plt.ylabel(r'$\textrm{log} ({\rm SFR} / M_{\odot}{\rm yr}^{-1})$')
+    plt.savefig(f'{sample_dir}{model}_{wind}_{snap}_cosi.png')
+    plt.clf()
+
+    im = plt.scatter(gal_sm, inclination, c=np.log10(gal_sfr + 1e-3), cmap=cmap, s=5, marker='o')
+    plt.colorbar(im, label=r'$\textrm{log} ({\rm SFR} / M_{\odot}{\rm yr}^{-1})$')
+    plt.clim(1.5, -3.5)
+    plt.xlim(9.75,11.75)
+    plt.ylim(0, 90)
+    plt.xlabel(r'$\log\ (M_{\star} / M_{\odot})$')
+    plt.ylabel(r'$i\ (^\circ)$')
+    plt.savefig(f'{sample_dir}{model}_{wind}_{snap}_mstar_inclination.png')
+    plt.clf()
+
+    im = plt.scatter(gal_sm, np.cos(alpha), c=np.log10(gal_sfr + 1e-3), cmap=cmap, s=5, marker='o')
+    plt.colorbar(im, label=r'$\textrm{log} ({\rm SFR} / M_{\odot}{\rm yr}^{-1})$')
+    plt.clim(1.5, -3.5)
+    plt.xlim(9.75,11.75)
+    plt.ylim(-1, 1)
+    plt.xlabel(r'$\log\ (M_{\star} / M_{\odot})$')
+    plt.ylabel(r'${\rm cos}\ i$')
+    plt.savefig(f'{sample_dir}{model}_{wind}_{snap}_mstar_cosi.png')
+    plt.clf()
+
+    plt.plot(sm_line, sf_line, ls='--', lw=1.3, c='dimgray')
+    plt.plot(sm_line, q_line, ls='--', lw=1.3, c='dimgray')
+    plt.text(11.55, 1.1, 'SF')
+    plt.text(11.55, 0.3, 'GV')
+    plt.text(11.55, -0.7, 'Q')
+    for i in range(nbins_m + 1):
+        plt.axvline(min_m+i*delta_m, ls=':', lw=1.5, c='darkgray')
+    im = plt.scatter(gal_sm, np.log10(gal_sfr + 1e-3), c=gal_fgas, cmap=cmap, s=5, marker='o')
+    plt.colorbar(im, label=r'$\textrm{log} (f_{\textrm{gas}})$')
+    plt.clim(1.0, -3.)
+    plt.xlim(9.75,11.75)
+    plt.ylim(-3.5, 1.5)
     plt.xlabel(r'$\log\ (M_{*} / M_{\odot})$')
     plt.ylabel(r'$\textrm{log} ({\rm SFR} / M_{\odot}{\rm yr}^{-1})$')
-    plt.savefig(f'{sample_dir}{model}_{wind}_{snap}_nocmap.png')
+    plt.savefig(f'{sample_dir}{model}_{wind}_{snap}_fgas.png')
     plt.clf()
 
