ff617fad680f76ac2814e5fc5a5f446b139eacfb
Sarah Appleby <sarahappleby20@gmail.com>
2022-02-03 17:16:02 +0000

    check chisq plots

--- absorption/ml_project/analyse_spectra/check_fits.py
+++ absorption/ml_project/analyse_spectra/check_fits.py
@@ -9,6 +9,22 @@ from spectrum import Spectrum
 from utils import *
 from physics import *
 
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', size=15)
+
+
+def make_chisq_range_plots(chisq, min_chisq, max_chisq, gal_ids, line, fr200, orients, spectra_dir, plot_dir, ngals=10):
+
+    chisq_index = np.where((chisq > min_chisq) & (chisq < max_chisq))
+    choice = np.random.choice(np.arange(len(chisq_index[0])), size=ngals, replace=False)
+    first, second = chisq_index[0][choice], chisq_index[1][choice]
+    for i in range(ngals):
+        spec_name = f'sample_galaxy_{gal_ids[first[i]]}_{line}_{orients[second[i]]}_{fr200}r200'
+        spec = Spectrum(f'{spectra_dir}{spec_name}.h5')
+        spec.plot_fit(filename = f'{plot_dir}/{spec_name}.png')
+
+
 if __name__ == '__main__':
 
     model = sys.argv[1]
@@ -31,17 +47,15 @@ if __name__ == '__main__':
     chisq_dict = read_h5_into_dict(chisq_file)
     max_chisq = chisq_dict[f'max_chisq_{fr200}r200'] 
 
-    low_chisq = np.where((max_chisq > 0.) & (max_chisq < 2.5))
-    choice = np.random.choice(np.arange(len(low_chisq[0])), size=ngals, replace=False)
-    first, second = low_chisq[0][choice], low_chisq[1][choice]
+    make_chisq_range_plots(max_chisq, 0., 2.5, gal_ids, line, fr200, orients, spectra_dir, 'chisq_plots/low_chisq/')
+    make_chisq_range_plots(max_chisq, 2.5, 5., gal_ids, line, fr200, orients, spectra_dir, 'chisq_plots/mid_chisq/')
+    make_chisq_range_plots(max_chisq, 5., np.inf, gal_ids, line, fr200, orients, spectra_dir, 'chisq_plots/high_chisq/')
+    make_chisq_range_plots(max_chisq, -99.5, -89.5, gal_ids, line, fr200, orients, spectra_dir, 'chisq_plots/no_features/')
 
-    for i in range(ngals):
-        spec_name = f'sample_galaxy_{gal_ids[first[i]]}_{line}_{orients[second[i]]}_{fr200}r200.h5'
-        spec = Spectrum(f'{spectra_dir}{spec_name}')
-        spec.plot_fit(f'plots/{spec_name}.png')
-    
-    mid_chisq = np.where((max_chisq > 2.5) & (max_chisq < 5.))
-    high_chisq = np.where((max_chisq > 5.))
-    no_features = np.where((max_chisq == -99.))
+    plt.hist(np.log10(chisq), bins=100, density=True, alpha=0.6)
+    plt.axvline(np.log10(2.5), ls='--', c='k')
+    plt.xlabel(r'${\rm log}\ \chi^2_r$')
+    plt.ylabel(r'${\rm Frequency}$')
+    plt.savefig(f'chisq_plots/chisq_hist_{line}_{fr200}r200.png')
+    plt.clf()
 
-     
