ab46da658761277d2b85400d60d503249bf3b5c0
Sarah Appleby <sarahappleby20@gmail.com>
2022-02-09 10:28:05 +0000

    ew plot for uvb test

---
+++ absorption/ml_project/analyse_spectra/equivalent_width_no_uvb.py
@@ -0,0 +1,68 @@
+import h5py
+import numpy as np
+import os
+import sys
+sys.path.insert(0, '/disk04/sapple/cgm/absorption/ml_project/make_spectra/')
+from utils import read_h5_into_dict
+
+def equivalent_width(flux, pixel_size):
+    return np.sum((np.ones(len(flux)) - flux) * pixel_size)
+
+
+if __name__ == '__main__':
+
+    model = sys.argv[1]
+    wind = sys.argv[2]
+    snap = sys.argv[3]
+    uvb = sys.argv[4] # can be 'uvb' or 'no_uvb'
+    minT = sys.argv[5] # can be 4.0, 4.5, 5.0, 5.5
+
+    delta_fr200 = 0.25
+    min_fr200 = 0.25
+    nbins_fr200 = 5
+    fr200 = np.arange(min_fr200, (nbins_fr200+1)*delta_fr200, delta_fr200)
+
+    # possible ions to choose:
+    lines = ['H1215', 'MgII2796', 'SiIII1206', 'CIV1548', 'OVI1031', 'NeVIII770']
+    orients = ['0_deg', '45_deg', '90_deg', '135_deg', '180_deg', '225_deg', '270_deg', '315_deg']
+    vel_range = 600. #km/s
+    bin_size = 6. # km/s 
+    c = 2.98e8 # km/s
+   
+    spectra_dir = f'/disk04/sapple/cgm/absorption/ml_project/data/collisional/{uvb}/{model}_{wind}_{snap}_minT_{minT}/'
+    sample_dir = f'/disk04/sapple/cgm/absorption/ml_project/data/samples/' 
+
+    with h5py.File(f'{sample_dir}{model}_{wind}_{snap}_galaxy_sample.h5', 'r') as sf:
+        gal_ids = sf['gal_ids'][:]
+
+    for line in lines:
+
+        results_file = f'/disk04/sapple/cgm/absorption/ml_project/data/collisional/results/{model}_{wind}_{snap}_{uvb}_minT_{minT}_ew_{line}.h5'
+
+        if os.path.isfile(results_file):
+            with h5py.File(results_file, 'r') as f:
+                all_keys = list(f.keys())
+        else:
+            all_keys = []
+
+        for i in range(len(fr200)):
+
+            if not f'ew_wave_{fr200[i]}r200' in all_keys:
+
+                all_ew = np.zeros((len(gal_ids), len(orients)))
+
+                for j, gal in enumerate(gal_ids):
+            
+                    for o, orient in enumerate(orients):
+
+                        spec_name = f'sample_galaxy_{gal_ids[j]}_{line}_{orient}_{fr200[i]}r200'
+                        spec = read_h5_into_dict(f'{spectra_dir}{spec_name}.h5')
+
+                        vel_mask = (spec['velocities'] < spec['gal_velocity_pos'] + vel_range) & (spec['velocities'] > spec['gal_velocity_pos'] - vel_range)
+                        flux = spec['fluxes'][vel_mask]
+                        pixel_size = spec['wavelengths'][1] - spec['wavelengths'][0]
+                        all_ew[j][o] = equivalent_width(flux, pixel_size)
+
+                with h5py.File(results_file, 'a') as f:
+                    f.create_dataset(f'ew_wave_{fr200[i]}r200', data=np.array(all_ew))
+            
---
+++ absorption/ml_project/analyse_spectra/plot_ew_no_uvb.py
@@ -0,0 +1,138 @@
+import numpy as np
+import h5py
+import sys
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from matplotlib import cm
+import caesar
+
+sys.path.insert(0, '/disk04/sapple/cgm/absorption/ml_project/make_spectra/')
+from utils import read_h5_into_dict
+sys.path.insert(0, '/disk04/sapple/tools/')
+from plotmedian import runningmedian
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', size=13)
+
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100, alpha=1.):
+        cmap_list = cmap(np.linspace(minval, maxval, n))
+        cmap_list[:, -1] = alpha
+        new_cmap = colors.LinearSegmentedColormap.from_list('trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+                                                            cmap_list)
+        return new_cmap
+
+def get_bin_middle(xbins):
+    return np.array([xbins[i] + 0.5*(xbins[i+1] - xbins[i]) for i in range(len(xbins)-1)])
+
+def make_color_list(cmap, nbins):
+    dc = 0.9 / (nbins -1)
+    frac = np.arange(0.05, 0.95+dc, dc)
+    return [cmap(i) for i in frac]
+
+if __name__ == '__main__':
+
+    model = sys.argv[1]
+    wind = sys.argv[2]
+    snap = sys.argv[3]
+
+    sim = caesar.load(f'/home/rad/data/{model}/{wind}/Groups/{model}_{snap}.hdf5')
+    redshift = sim.simulation.redshift
+
+    line = 'H1215'
+    plot_line = r'${\rm HI}1215$'
+    norients = 8
+    fr200 = 0.25
+    minT = ['4.0', '4.5', '5.0', '5.5']
+   
+    delta_m = 0.25
+    min_m = 10.
+    nbins_m = 5
+    mass_bins = np.arange(min_m, min_m+(nbins_m+1)*delta_m, delta_m)
+    plot_bins = get_bin_middle(mass_bins)
+
+    ssfr_cmap = plt.get_cmap('jet_r')
+    ssfr_cmap = truncate_colormap(ssfr_cmap, 0.1, 1.0)
+    colors = make_color_list(plt.get_cmap('viridis'), nbins_m)
+
+    plot_dir = f'/disk04/sapple/cgm/absorption/ml_project/analyse_spectra/plots/'
+    results_dir = f'/disk04/sapple/cgm/absorption/ml_project/data/collisional/results/'
+    sample_dir = f'/disk04/sapple/cgm/absorption/ml_project/data/samples/'
+    with h5py.File(f'{sample_dir}{model}_{wind}_{snap}_galaxy_sample.h5', 'r') as sf:
+        gal_ids = sf['gal_ids'][:]
+        gal_sm = sf['mass'][:]
+        gal_sfr = sf['sfr'][:]
+        gal_ssfr = sf['ssfr'][:] + 9.
+
+    mass_long = np.repeat(gal_sm, norients)
+    sfr_long = np.repeat(np.log10(gal_sfr + 1e-3), norients)
+    ssfr_long = np.repeat(gal_ssfr, norients)
+
+    # Median difference plots :)
+
+    for i in range(len(minT)):
+
+        no_uvb_dict = read_h5_into_dict(f'{results_dir}{model}_{wind}_{snap}_no_uvb_minT_{minT[i]}_ew_{line}.h5')
+        with_uvb_dict = read_h5_into_dict(f'{results_dir}{model}_{wind}_{snap}_with_uvb_minT_{minT[i]}_ew_{line}.h5')
+
+        ew_no_uvb = no_uvb_dict[f'ew_wave_{fr200}r200'].flatten()
+        ew_with_uvb = with_uvb_dict[f'ew_wave_{fr200}r200'].flatten()
+
+        median = np.zeros(nbins_m)
+        per25 = np.zeros(nbins_m)
+        per75 = np.zeros(nbins_m)
+
+        for j in range(nbins_m):
+
+            mask = (mass_long > mass_bins[j]) & (mass_long < mass_bins[j+1])
+            median[j] = np.nanmedian(np.log10(ew_no_uvb[mask]) - np.log10(ew_with_uvb[mask]))
+            per25[j] = np.nanpercentile(np.log10(ew_no_uvb[mask]) - np.log10(ew_with_uvb[mask]), 25.)
+            per75[j] = np.nanpercentile(np.log10(ew_no_uvb[mask]) - np.log10(ew_with_uvb[mask]), 75.)
+
+        plt.plot(plot_bins, median, ls='-', c=colors[i], label=r'$T_{{\rm min}} = {{{}}}$'.format(minT[i]))
+        if i == len(minT) -1:
+            plt.fill_between(plot_bins, per25, per75, color=colors[i], alpha=0.4)
+    
+    plt.legend()
+    plt.xlabel(r'$\log\ (M_{*} / M_{\odot})$')
+    plt.ylabel(r'$\Delta {\rm log (EW}/\AA)$')
+    plt.ylim(-1, 1)
+
+    plt.tight_layout()
+    plt.savefig(f'{plot_dir}{model}_{wind}_{snap}_uvb_test_mass_median_delta_ew_{line}.png')
+    plt.show()
+    plt.clf()
+
+
+    """
+    # Difference plots, coloured by sSFR :)
+
+    fig, ax = plt.subplots(1, len(minT), figsize=(10, 6), sharey='row', sharex='col')
+    ax = ax.flatten()
+
+    for i in range(len(minT)):
+
+        no_uvb_dict = read_h5_into_dict(f'{results_dir}{model}_{wind}_{snap}_no_uvb_minT_{minT[i]}_ew_{line}.h5')
+        with_uvb_dict = read_h5_into_dict(f'{results_dir}{model}_{wind}_{snap}_with_uvb_minT_{minT[i]}_ew_{line}.h5')
+
+        ew_no_uvb = no_uvb_dict[f'ew_wave_{fr200}r200'].flatten()
+        ew_with_uvb = with_uvb_dict[f'ew_wave_{fr200}r200'].flatten()
+
+        im = ax[i].scatter(mass_long, np.log10(ew_no_uvb) - np.log10(ew_with_uvb), c=ssfr_long, cmap=ssfr_cmap, s=1.5, vmin=-2.5, vmax=0)
+        ax[i].set_xlabel(r'$\log\ (M_{*} / M_{\odot})$')
+        if i == 0:
+            ax[i].set_ylabel(r'$\Delta {\rm log (EW}_{\rm HI}/\AA)$')
+        if i == len(minT) -1:
+            ax[i].annotate(plot_line, xy=(0.05, 0.85), xycoords='axes fraction')
+        ax[i].set_title(r'$T_{{\rm min}} = {{{}}}$'.format(minT[i]))
+        ax[i].set_ylim(-1, 1)
+
+    fig.subplots_adjust(right=0.8, bottom=0.1, top=0.9)
+    cbar_ax = fig.add_axes([0.9, 0.15, 0.05, 0.7])
+    cbar = fig.colorbar(im,ax=cbar_ax, shrink=.6, label=r'$\log\ ({\rm sSFR} / {\rm Gyr}^{-1})$')
+
+    #plt.tight_layout()
+    fig.subplots_adjust(wspace=0., hspace=0.)
+    plt.savefig(f'{plot_dir}{model}_{wind}_{snap}_uvb_test_mass_delta_ew_{line}.png')
+    plt.clf()
+    """
+
