e5f0d72721c03af54338a78dcd44c7b69f2de780
Sarah Appleby <sarahappleby20@gmail.com>
2022-02-21 15:21:29 +0000

    satellite gal properties and radii plot

---
+++ absorption/ml_project/make_spectra/tasks/median_rhalf_sats.py
@@ -0,0 +1,39 @@
+import caesar
+import h5py
+import numpy as np
+
+model = 'm100n1024'
+wind = 's50'
+snap = '151'
+
+data_dir = f'/home/rad/data/{model}/{wind}/Groups/'
+sim = caesar.load(f'{data_dir}{model}_{snap}.hdf5')
+
+delta_m = 0.25
+min_m = 9.
+nbins_m = 10.
+mass_bins = np.arange(min_m, min_m+nbins_m*delta_m, delta_m)
+
+mass_plot_titles = []
+for i in range(nbins_m):
+    mass_plot_titles.append(f'{mass_bins[i]}'+ r'$ < \textrm{log} (M_* / M_{\odot}) < $' + f'{mass_bins[i]+delta_m}')
+
+
+sat_mask = ~np.array([i.central for i in sim.galaxies])
+sat_ids = np.arange(len(sim.galaxies))[sat_mask]
+
+h = sim.simulation.hubble_constant
+redshift = sim.simulation.redshift
+
+rhalf = np.array([sim.galaxies[i].radii['stellar_half_mass'].in_units('kpc/h') for i in sat_ids]) * (1+redshift)
+mass = np.log10([sim.galaxies[i].masses['stellar'].in_units('Msun') for i in sat_ids])
+
+median_rhalf = np.zeros(len(mass_bins))
+ngals = np.zeros(len(mass_bins))
+for i in range(len(mass_bins)):
+    mass_mask = (mass > mass_bins[i]) & (mass < mass_bins[i]+ delta_m)
+    median_rhalf[i] = np.around(np.nanmedian(rhalf[mass_mask]), 2)
+    ngals[i] = len(rhalf[mass_mask])
+
+print(mass_bins)
+print(median_rhalf_all)
---
+++ absorption/ml_project/make_spectra/tasks/satellite_radius_test.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+from matplotlib.collections import PatchCollection
+import numpy as np
+import sys
+
+import caesar
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', size=13)
+
+
+if __name__ == '__main__':
+
+    model = sys.argv[1]
+    wind = sys.argv[2]
+    snap = sys.argv[3]
+
+    log_frad_min = 0.5
+    log_frad_max = 3.0
+    log_dfrad = 0.5
+    log_frad = np.arange(log_frad_min, log_frad_max+log_dfrad, log_dfrad)
+
+    data_dir = f'/home/rad/data/{model}/{wind}/'
+    sim =  caesar.load(f'{data_dir}Groups/{model}_{snap}.hdf5')
+    boxsize = sim.simulation.boxsize.in_units('kpc/h')
+
+    central = np.array([i.central for i in sim.galaxies])
+    gal_pos = np.array([i.pos.in_units('unitary') for i in sim.galaxies])[~central]
+    gal_rad = np.array([i.radii['stellar_half_mass'].in_units('kpc/h') for i in sim.galaxies])[~central] / boxsize
+
+    y_pos = gal_pos[:, 1]
+    z_pos = gal_pos[:, 2]
+
+    fig, ax = plt.subplots(2, 3, sharey='row', sharex='col')
+    ax = ax.flatten()
+
+    for i in range(len(log_frad)):
+
+        rad = gal_rad * 10**log_frad[i]
+
+        patches = []
+        for j in range(len(gal_rad)):
+            patches.append( Circle( (x_pos[j], y_pos[j]), rad[j]) )
+
+        p = PatchCollection(patches, alpha=0.25)
+        ax[i].add_collection(p)
+
+        ax[i].annotate(r'${{\rm log}} f_{{r_{{\rm half}} \star}} = {{{}}}$'.format(log_frad[i]), xy=(0.05, 0.05), xycoords='axes fraction')
+
+        if i in [3, 4, 5]:
+            ax[i].set_xlabel('x (unitary)')
+        if i in [0, 3]:
+            ax[i].set_ylabel('y (unitary)')
+
+    plt.tight_layout()
+    fig.subplots_adjust(wspace=0., hspace=0.)
+    plt.savefig('satellite_only.png')
+    plt.show()
