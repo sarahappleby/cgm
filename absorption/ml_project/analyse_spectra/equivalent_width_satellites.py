updated sat plots and correct Zsolar units

absorption/ml_project/analyse_spectra/equivalent_width_satellites.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     fr200 = np.arange(min_fr200, (nbins_fr200+1)*delta_fr200, delta_fr200)
 
     # possible ions to choose:
-    lines = ['H1215', 'MgII2796', 'SiIII1206', 'CIV1548', 'OVI1031', 'NeVIII770']
+    lines = ['H1215', 'MgII2796', 'CII1334', 'SiIII1206', 'CIV1548', 'OVI1031']
     orients = ['0_deg', '45_deg', '90_deg', '135_deg', '180_deg', '225_deg', '270_deg', '315_deg']
     vel_range = 600. #km/s
     bin_size = 6. # km/s 

absorption/ml_project/analyse_spectra/more_plots/plot_Nweighted_nTZ_massbins_solo.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     ion_mass = np.array([pg.UnitArr(pg.analysis.absorption_spectra.lines[line]['atomwt']) * pg.physics.m_u for line in lines])
     chisq_lim = 2.5
     N_min = 12.
-    zsolar = 0.0134
+    zsolar = [0.0134, 7.14e-4, 2.38e-3, 6.71e-4, 2.38e-3, 5.79e-3]
 
     delta_fr200 = 0.25
     min_fr200 = 0.25
@@ -63,7 +63,7 @@ if __name__ == '__main__':
             weighted_Z = np.zeros(nbins_m)
 
             with h5py.File(results_file, 'r') as hf:
-                all_Z = hf[f'log_Z_{fr200[i]}r200'][:] - np.log10(zsolar)
+                all_Z = hf[f'log_Z_{fr200[i]}r200'][:] - np.log10(zsolar[l])
                 all_T = hf[f'log_T_{fr200[i]}r200'][:]
                 all_n = hf[f'log_rho_{fr200[i]}r200'][:] - np.log10(ion_mass[l])
                 all_N = hf[f'log_N_{fr200[i]}r200'][:]

absorption/ml_project/analyse_spectra/plot_Nweighted_nTZ.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     ion_mass = np.array([pg.UnitArr(pg.analysis.absorption_spectra.lines[line]['atomwt']) * pg.physics.m_u for line in lines])
     chisq_lim = 2.5
     N_min = 12.
-    zsolar = 0.0134
+    zsolar = [0.0134, 7.14e-4, 2.38e-3, 6.71e-4, 2.38e-3, 5.79e-3]
 
     delta_fr200 = 0.25
     min_fr200 = 0.25
@@ -62,7 +62,7 @@ if __name__ == '__main__':
         for i in range(len(fr200)):
 
             with h5py.File(results_file, 'r') as hf:
-                all_Z = hf[f'log_Z_{fr200[i]}r200'][:] - np.log10(zsolar)
+                all_Z = hf[f'log_Z_{fr200[i]}r200'][:] - np.log10(zsolar[l])
                 all_T = hf[f'log_T_{fr200[i]}r200'][:]
                 all_n = hf[f'log_rho_{fr200[i]}r200'][:] - np.log10(ion_mass[l])
                 all_N = hf[f'log_N_{fr200[i]}r200'][:]
@@ -92,17 +92,17 @@ if __name__ == '__main__':
             if i == 0:
                 ax[0].errorbar(line_ev[l], weighted_n[i], color=colors[i], yerr=np.array([[weighted_n[i] - weighted_n_25[i], weighted_n_75[i] - weighted_n[i],]]).T,
                                   lw=1, ls='None', marker='None', capsize=2)
-            if l == 0:
-                ax[0].scatter(line_ev[l], weighted_n[i], color=colors[i], label=r'$\rho / r_{{200}} = {{{}}}$'.format(fr200[i]))
-            else:
-                ax[0].scatter(line_ev[l], weighted_n[i], color=colors[i])
 
+            ax[0].scatter(line_ev[l], weighted_n[i], color=colors[i])
             ax[1].scatter(line_ev[l], weighted_T[i], color=colors[i])
-            ax[2].scatter(line_ev[l], weighted_Z[i], color=colors[i])
-    
+            if l == 0:
+                ax[2].scatter(line_ev[l], weighted_Z[i], color=colors[i], label=r'$\rho / r_{{200}} = {{{}}}$'.format(fr200[i]))
+            else:
+                ax[2].scatter(line_ev[l], weighted_Z[i], color=colors[i])
+
         ax[0].annotate(plot_lines[l], xy=(line_ev[l] - adjust_x[l], np.min(weighted_n - 0.45)))
 
-    ax[0].legend(loc=1, fontsize=12)
+    ax[2].legend(loc=4, fontsize=12)
     ax[0].set_ylim(-3.75, -0.5)
 
     ax[2].set_xlabel(r'${\rm log }(E / {\rm eV})$')

absorption/ml_project/analyse_spectra/plot_diffew_profile_satellites.py
@@ -28,16 +28,16 @@ if __name__ == '__main__':
     sim = caesar.load(f'/home/rad/data/{model}/{wind}/Groups/{model}_{snap}.hdf5')
     redshift = sim.simulation.redshift
 
-    lines = ["H1215", "MgII2796", "SiIII1206", "CIV1548", "OVI1031", "NeVIII770"]
-    plot_lines = [r'${\rm HI}1215$', r'${\rm MgII}2796$', r'${\rm SiIII}1206$', 
-                  r'${\rm CIV}1548$', r'${\rm OVI}1031$', r'${\rm NeVIII}770$']
+    lines = ["H1215", "MgII2796", "CII1334", "SiIII1206", "CIV1548", "OVI1031"]
+    plot_lines = [r'${\rm HI}1215$', r'${\rm MgII}2796$', r'${\rm CII}1334$',
+                  r'${\rm SiIII}1206$', r'${\rm CIV}1548$', r'${\rm OVI}1031$']
     plot_quantities = ['med', 'per25', 'per75',]
     norients = 8
     delta_fr200 = 0.25 
     min_fr200 = 0.25 
     nbins_fr200 = 5 
     fr200 = np.arange(min_fr200, (nbins_fr200+1)*delta_fr200, delta_fr200)
-    log_frad = ['0.0', '0.5', '1.0', '1.5', '2.0'] 
+    log_frad = ['0.0', '0.5', '1.0', '1.5', '2.0', '2.5'] 
 
     delta_m = 0.5
     min_m = 10.5
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     normal_dir = f'/disk04/sapple/cgm/absorption/ml_project/data/normal/results/'
     plot_dir = '/disk04/sapple/cgm/absorption/ml_project/analyse_spectra/plots/'
     sample_dir = f'/disk04/sapple/cgm/absorption/ml_project/data/samples/'
-    with h5py.File(f'{sample_dir}{model}_{wind}_{snap}_galaxy_sample_old.h5', 'r') as sf:
+    with h5py.File(f'{sample_dir}{model}_{wind}_{snap}_galaxy_sample.h5', 'r') as sf:
         mass_long = np.repeat(sf['mass'][:], norients)
 
     fig, ax = plt.subplots(2, 3, figsize=(14, 13), sharey='row', sharex='col')
@@ -95,7 +95,7 @@ if __name__ == '__main__':
 
             ax[l].plot(plot_data['fr200'], plot_data[f'log_frad_{log_frad[i]}_{bin_label}_med'], 
                        ls='-', c=colors[i], label=r'${{\rm log}} f_{{r_{{\rm half}} \star}} = {{{}}}$'.format(log_frad[i]), lw=1.5)
-            if i == 1:
+            if log_frad[i] == '1.0':
                 ax[l].fill_between(plot_data['fr200'], plot_data[f'log_frad_{log_frad[i]}_{bin_label}_per75'], plot_data[f'log_frad_{log_frad[i]}_{bin_label}_per25'], 
                                       alpha=0.3, color=colors[i])
 
@@ -104,7 +104,7 @@ if __name__ == '__main__':
 
         if l in [0, 3]:
             ax[l].set_ylabel(r'${\rm log }( {\rm EW}_{\rm sat} / {\rm EW}_{\rm total} )$')
-        if l == 0:
+        if l == 2:
             ax[l].legend(loc=4)
         if l in [3, 4, 5]:
             ax[l].set_xlabel(r'$\rho / r_{200}$')

absorption/ml_project/analyse_spectra/plot_diffew_satellites.py
@@ -26,14 +26,13 @@ if __name__ == '__main__':
     wind = sys.argv[2]
     snap = sys.argv[3]
 
-    lines = ['H1215', 'MgII2796', 'SiIII1206', 'CIV1548', 'OVI1031', 'NeVIII770']
-    plot_lines = [r'${\rm HI}1215$', r'${\rm MgII}2796$', r'${\rm SiIII}1206$',
-                  r'${\rm CIV}1548$', r'${\rm OVI}1031$', r'${\rm NeVIII}770$']
+    lines = ['H1215', 'MgII2796', 'CII1334', 'SiIII1206', 'CIV1548', 'OVI1031']
+    plot_lines = [r'${\rm HI}1215$', r'${\rm MgII}2796$', r'${\rm CII}1334$',
+                  r'${\rm SiIII}1206$', r'${\rm CIV}1548$', r'${\rm OVI}1031$']
     plot_quantities = ['med', 'per25', 'per75']
     norients = 8
     fr200 = 1.0
-    log_frad = ['0.0', '0.5', '1.0', '1.5', '2.0', '2.5', '3.0']
-    log_frad = ['0.0', '0.5', '1.0', '1.5', '2.0']
+    log_frad = ['0.0', '0.5', '1.0', '1.5', '2.0', '2.5']
 
     delta_m = 0.25
     min_m = 10.
@@ -46,7 +45,7 @@ if __name__ == '__main__':
     results_dir = f'/disk04/sapple/cgm/absorption/ml_project/data/satellites/results/'
     sample_dir = f'/disk04/sapple/cgm/absorption/ml_project/data/samples/'
 
-    with h5py.File(f'{sample_dir}{model}_{wind}_{snap}_galaxy_sample_old.h5', 'r') as sf: # change this back to current sample
+    with h5py.File(f'{sample_dir}{model}_{wind}_{snap}_galaxy_sample.h5', 'r') as sf: # change this back to current sample
         mass_long = np.repeat(sf['mass'][:], norients)
 
     fig, ax = plt.subplots(2, 3, figsize=(15, 10), sharex='col', sharey='row')
@@ -89,7 +88,7 @@ if __name__ == '__main__':
 
         for i in range(len(log_frad)):
             ax[l].plot(plot_data['mass'], plot_data[f'log_frad_{log_frad[i]}_med'], ls='-', c=colors[i], label=r'${{\rm log}} f_{{r_{{\rm half}} \star}} = {{{}}}$'.format(log_frad[i]))
-            if i == 1:
+            if log_frad[i] == '1.0':
                 ax[l].fill_between(plot_data['mass'], plot_data[f'log_frad_{log_frad[i]}_per25'], plot_data[f'log_frad_{log_frad[i]}_per75'], color=colors[i], alpha=0.4)
     
         if l == 0:

absorption/ml_project/analyse_spectra/plot_phase_space.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     ion_mass = np.array([pg.UnitArr(pg.analysis.absorption_spectra.lines[line]['atomwt']) * pg.physics.m_u for line in lines])
     chisq_lim = 2.5
     N_min = 12.
-    zsolar = 0.0134
+    zsolar = [0.0134, 7.14e-4, 2.38e-3, 6.71e-4, 2.38e-3, 5.79e-3]
 
     delta_fr200 = 0.25
     min_fr200 = 0.25
@@ -44,7 +44,7 @@ if __name__ == '__main__':
         for i in range(len(fr200)):
 
             with h5py.File(results_file, 'r') as hf:
-                all_Z = hf[f'log_Z_{fr200[i]}r200'][:] - np.log10(zsolar)
+                all_Z = hf[f'log_Z_{fr200[i]}r200'][:] - np.log10(zsolar[l])
                 all_T = hf[f'log_T_{fr200[i]}r200'][:]
                 all_rho = hf[f'log_rho_{fr200[i]}r200'][:]
                 all_N = hf[f'log_N_{fr200[i]}r200'][:]
@@ -58,9 +58,9 @@ if __name__ == '__main__':
             all_ids = all_ids[mask]
             all_N = all_N[mask]
 
-            im = ax[l][i].scatter(all_rho, all_T, c=all_Z, cmap='magma', s=1, vmin=-2, vmax=0)
+            im = ax[l][i].scatter(all_rho, all_T, c=all_Z, cmap='magma', s=1, vmin=-1., vmax=0.5)
             ax[l][i].set_xlim(-6, 0)
-            ax[l][i].set_ylim(2, 8)
+            ax[l][i].set_ylim(3, 8)
 
             if i == len(fr200) -1:
                 fig.colorbar(im, ax=ax[l][i], label=r'${\rm log} (Z / Z_{\odot})$')
@@ -87,7 +87,7 @@ if __name__ == '__main__':
         for i in range(len(fr200)):
 
             with h5py.File(results_file, 'r') as hf:
-                all_Z = hf[f'log_Z_{fr200[i]}r200'][:] - np.log10(zsolar)
+                all_Z = hf[f'log_Z_{fr200[i]}r200'][:] - np.log10(zsolar[l])
                 all_T = hf[f'log_T_{fr200[i]}r200'][:]
                 all_rho = hf[f'log_rho_{fr200[i]}r200'][:]
                 all_N = hf[f'log_N_{fr200[i]}r200'][:]
@@ -103,7 +103,7 @@ if __name__ == '__main__':
 
             im = ax[l][i].scatter(all_rho, all_T, c=all_N, cmap='magma', s=1, vmin=12, vmax=16)
             ax[l][i].set_xlim(-6, 0)
-            ax[l][i].set_ylim(2, 8)
+            ax[l][i].set_ylim(3, 8)
 
             if i == len(fr200) -1:
                 fig.colorbar(im, ax=ax[l][i], label=r'${\rm log }(N / {\rm cm}^{-2})$')